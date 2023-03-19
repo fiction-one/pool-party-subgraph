a89e4d5c68bd739f76d40fd8e99ff2d7d0c459ce
Nicky Ru <64008830+nicky-ru@users.noreply.github.com>
2023-03-19 20:16:10 +0100

    update IdRegistry handlers

--- src/id-registry.ts
+++ src/id-registry.ts
@@ -1,15 +1,5 @@
 import { FID } from "./../generated/schema";
-import {
-  CancelRecovery,
-  ChangeHome,
-  ChangeRecoveryAddress,
-  ChangeTrustedCaller,
-  DisableTrustedOnly,
-  OwnershipTransferred,
-  Register,
-  RequestRecovery,
-  Transfer,
-} from "../generated/IdRegistry/IdRegistry";
+import { Register, Transfer } from "../generated/IdRegistry/IdRegistry";
 
 export function handleRegister(event: Register): void {
   let fid = new FID(event.params.id.toString());
@@ -19,21 +9,5 @@ export function handleRegister(event: Register): void {
   fid.createdAtTimestamp = event.block.timestamp;
   fid.save();
 }
-
-export function handleCancelRecovery(event: CancelRecovery): void {}
-
-export function handleChangeHome(event: ChangeHome): void {}
-
-export function handleChangeRecoveryAddress(
-  event: ChangeRecoveryAddress
-): void {}
-
-export function handleChangeTrustedCaller(event: ChangeTrustedCaller): void {}
-
-export function handleDisableTrustedOnly(event: DisableTrustedOnly): void {}
-
-export function handleOwnershipTransferred(event: OwnershipTransferred): void {}
-
-export function handleRequestRecovery(event: RequestRecovery): void {}
 
 export function handleTransfer(event: Transfer): void {}
