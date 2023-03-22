2147e3429b7b440ce8a8745e499374f6b8095740
Nicky Ru <64008830+nicky-ru@users.noreply.github.com>
2023-03-22 18:52:11 +0100

    Update fname.test.ts

--- tests/fname/fname.test.ts
+++ tests/fname/fname.test.ts
@@ -1,18 +1,39 @@
-import { test, describe, assert } from "matchstick-as/assembly/index";
+import {
+  test,
+  describe,
+  assert,
+  beforeAll,
+  logStore,
+} from "matchstick-as/assembly/index";
 
 import { createTransferEvent } from "./utils";
 import { handleTransfer } from "../../src/name-registry";
 
-describe("NameRegistry", () => {
-  test("handleTransfer should work", () => {
-    const from = "0x0000000000000000000000000000000000000000";
-    const to = "0x39ff405821ece5c94e976f3d6ac676f125976403";
-    const tokenId = "21923710825338253839304839389520962719014825665598212984806875314222184529920"
+const ZERO_ADDRESS = "0x0000000000000000000000000000000000000000";
 
-    let transferEvent = createTransferEvent(from, to, tokenId);
+let tokenId1 = "";
+let tokenId2 = "";
+let custodyAddr1 = "";
+let custodyAddr2 = "";
+
+describe("NameRegistry", () => {
+  beforeAll(() => {
+    tokenId1 =
+      "21923710825338253839304839389520952719014825665598212984806875314222184529920";
+    tokenId2 =
+      "21923710825338253839304839389520952719014825665598212984806875314222184529921";
+
+    custodyAddr1 = "0x39ff405821ece5c94e976f3d6ac676f125976303";
+    custodyAddr2 = "0x39ff405821ece5c94e976f3d6ac676f125976304";
+  });
+  test("handleTransfer should work", () => {
+    const from = ZERO_ADDRESS;
+
+    let transferEvent = createTransferEvent(from, custodyAddr1, tokenId1);
 
     handleTransfer(transferEvent);
 
-    assert.fieldEquals("FName", tokenId, "custodyAddr", to);
+    assert.fieldEquals("FName", tokenId1, "custodyAddr", custodyAddr1);
+    logStore();
   });
 });
