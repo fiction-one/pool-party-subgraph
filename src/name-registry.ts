dd117ca11c3e68a561a8821cecc9a205eb00e231
Nicky Ru <64008830+nicky-ru@users.noreply.github.com>
2023-03-22 13:11:19 +0100

    Update name-registry.ts

--- src/name-registry.ts
+++ src/name-registry.ts
@@ -1,11 +1,50 @@
+import { FName } from "./../generated/schema";
 import {
-  AdminChanged as AdminChangedEvent,
-  BeaconUpgraded as BeaconUpgradedEvent,
-  Upgraded as UpgradedEvent,
+  Renew as RenewEvent,
+  Transfer as TransferEvent,
+  ReclaimCall,
+  RegisterCall,
+  TrustedRegisterCall,
 } from "../generated/NameRegistry/NameRegistry";
 
-export function handleAdminChanged(event: AdminChangedEvent): void {}
+export function handleTransfer(event: TransferEvent): void {
+  // from Address
+  // to Address
+  // tokenId bigInt
+  let fname = FName.load(event.params.tokenId.toString());
 
-export function handleBeaconUpgraded(event: BeaconUpgradedEvent): void {}
+  if (fname) {
+    fname.custodyAddr = event.params.to;
+    fname.save();
+  } else {
+    fname = new FName(event.params.tokenId.toString());
+    fname.custodyAddr = event.params.to;
+    fname.createdAtBlock = event.block.number;
+    fname.createdAtTimestamp = event.block.timestamp;
+    fname.save();
+  }
+}
 
-export function handleUpgraded(event: UpgradedEvent): void {}
+export function handleRegister(call: RegisterCall): void {
+  // fname string
+  // to Address
+  // secret string
+  // recovery Address
+}
+
+export function handleRenew(event: RenewEvent): void {
+  // tokenId bigInt
+  // expiry bigInt
+}
+
+export function handleReclaim(call: ReclaimCall): void {
+  // tokenId bigInt
+}
+
+export function handleTrustedRegister(call: TrustedRegisterCall): void {
+  // fname string
+  // to Address
+  // recovery Address
+  // inviter bigInt
+  // invitee bigInt
+}
