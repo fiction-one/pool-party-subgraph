d16672338d70864f6f107422e655385b8b907f32
Nicky Ru <64008830+nicky-ru@users.noreply.github.com>
2023-03-23 17:27:15 +0100

    implement and test fid mapping

--- src/id-registry.ts
+++ src/id-registry.ts
@@ -4,10 +4,16 @@ import { Register, Transfer } from "../generated/IdRegistry/IdRegistry";
 export function handleRegister(event: Register): void {
   let fid = new FID(event.params.id.toString());
   fid.custodyAddr = event.params.to;
-  fid.recoveryAddr = event.params.recovery;
   fid.createdAtBlock = event.block.number;
   fid.createdAtTimestamp = event.block.timestamp;
   fid.save();
 }
 
-export function handleTransfer(event: Transfer): void {}
+export function handleTransfer(event: Transfer): void {
+  let fid = FID.load(event.params.id.toString());
+
+  if (fid) {
+    fid.custodyAddr = event.params.to;
+    fid.save();
+  }
+}
---
+++ tests/fid/fid.test.ts
@@ -0,0 +1,69 @@
+import {
+  describe,
+  test,
+  assert,
+  clearStore,
+  logStore,
+  afterEach,
+  beforeAll,
+} from "matchstick-as/assembly/index";
+
+import { createRegisterEvent, createTransferEvent } from "./utils";
+import { handleRegister, handleTransfer } from "../../src/id-registry";
+import { Register as RegisterEvent } from "../../generated/IdRegistry/IdRegistry";
+
+const custodyAddr1 = "0x39ff405821ece5c94e976f3d6ac676f125976303";
+const custodyAddr2 = "0x39ff405821ece5c94e976f3d6ac676f125976305";
+const recoveryAddr = "0x39ff405821ece5c94e976f3d6ac676f125976304";
+const fid1 = "1";
+const testUrl = "test";
+
+let registerEventGlobal: RegisterEvent;
+
+describe("Farcaster IDs", () => {
+  describe("Register Event", () => {
+    afterEach(() => {
+      clearStore();
+    });
+    test("should register new fid", () => {
+      const registerEvent = createRegisterEvent(
+        custodyAddr1,
+        fid1,
+        recoveryAddr,
+        testUrl
+      );
+      handleRegister(registerEvent);
+
+      assert.fieldEquals("FID", fid1, "custodyAddr", custodyAddr1);
+      assert.fieldEquals("FID", fid1, "createdAtBlock", "1");
+      assert.fieldEquals("FID", fid1, "createdAtTimestamp", "1");
+    });
+  });
+  describe("Transfer Event", () => {
+    beforeAll(() => {
+      registerEventGlobal = createRegisterEvent(
+        custodyAddr1,
+        fid1,
+        recoveryAddr,
+        testUrl
+      );
+    });
+    afterEach(() => {
+      logStore();
+      clearStore();
+    });
+    test("should handle fid transfer", () => {
+      handleRegister(registerEventGlobal);
+      const transferEvent = createTransferEvent(
+        custodyAddr1,
+        custodyAddr2,
+        fid1
+      );
+      handleTransfer(transferEvent);
+
+      assert.fieldEquals("FID", fid1, "custodyAddr", custodyAddr2);
+      assert.fieldEquals("FID", fid1, "createdAtBlock", "1");
+      assert.fieldEquals("FID", fid1, "createdAtTimestamp", "1");
+    });
+  });
+});
