4a6c69c9c7c0ca3cba57cdd4ef3af0364d74f66a
Nicky Ru <64008830+nicky-ru@users.noreply.github.com>
2023-03-23 20:20:52 +0100

    cleanup name registry

--- src/name-registry.ts
+++ src/name-registry.ts
@@ -1,56 +1,26 @@
-import { BigInt, Bytes } from "@graphprotocol/graph-ts";
-
-import { FName, User } from "./../generated/schema";
+import { FName } from "./../generated/schema";
 import {
   Renew as RenewEvent,
   Transfer as TransferEvent,
 } from "../generated/NameRegistry/NameRegistry";
+import { updateUserFnameId, deleteUserFnameId, initializeFname } from "./utils";
 
 export function handleTransfer(event: TransferEvent): void {
-  let fname = FName.load(event.params.tokenId.toString());
+  const tokenId = event.params.tokenId;
+  const newCustody = event.params.to;
+  const oldCustody = event.params.from;
+
+  let fname = FName.load(tokenId.toString());
 
   if (!fname) {
-    fname = new FName(event.params.tokenId.toString());
-    fname.fname = extractFname(event.params.tokenId);
-    fname.createdAtBlock = event.block.number;
-    fname.createdAtTimestamp = event.block.timestamp;
+    fname = initializeFname(event);
   }
 
-  fname.custodyAddr = event.params.to;
+  fname.custodyAddr = newCustody;
   fname.save();
 
-  handleOldOwner(event.params.from.toHex());
-  handleNewOwner(event.params.to.toHex(), event.params.tokenId.toString());
-}
-
-function handleNewOwner(to: string, tokenId: string): void {
-  let user = User.load(to);
-
-  if (!user) {
-    user = new User(to);
-    user.fid = "0";
-  }
-
-  user.fname = tokenId;
-  user.save();
-}
-
-function handleOldOwner(from: string): void {
-  let user = User.load(from);
-
-  if (!user) {
-    user = new User(from);
-    user.fid = "0";
-  }
-
-  user.fname = "";
-  user.save();
-}
-
-function extractFname(tokenId: BigInt): string {
-  const tokenIdHex = tokenId.toHexString();
-  const tokenIdBytes = Bytes.fromHexString(tokenIdHex);
-  return tokenIdBytes.toString();
+  deleteUserFnameId(oldCustody.toHex());
+  updateUserFnameId(newCustody.toHex(), tokenId.toString());
 }
 
 export function handleRenew(event: RenewEvent): void {
---
+++ src/utils.ts
@@ -0,0 +1,45 @@
+import { FName } from "./../generated/schema";
+import { Transfer } from "./../generated/NameRegistry/NameRegistry";
+import { BigInt, Bytes } from "@graphprotocol/graph-ts";
+import { User } from "../generated/schema";
+
+export function initializeFname(event: Transfer): FName {
+  const tokenId = event.params.tokenId;
+
+  const fname = new FName(tokenId.toString());
+  fname.fname = extractNameFromId(tokenId);
+  fname.createdAtBlock = event.block.number;
+  fname.createdAtTimestamp = event.block.timestamp;
+
+  return fname;
+}
+
+export function updateUserFnameId(to: string, fnameId: string): void {
+  let user = User.load(to);
+
+  if (!user) {
+    user = new User(to);
+    user.fid = "0";
+  }
+
+  user.fname = fnameId;
+  user.save();
+}
+
+export function deleteUserFnameId(from: string): void {
+  let user = User.load(from);
+
+  if (!user) {
+    user = new User(from);
+    user.fid = "0";
+  }
+
+  user.fname = "";
+  user.save();
+}
+
+export function extractNameFromId(tokenId: BigInt): string {
+  const tokenIdHex = tokenId.toHexString();
+  const tokenIdBytes = Bytes.fromHexString(tokenIdHex);
+  return tokenIdBytes.toString();
+}
