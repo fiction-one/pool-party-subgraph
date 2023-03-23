4818a398183e50c98ca588939248709a28299ef7
Nicky Ru <64008830+nicky-ru@users.noreply.github.com>
2023-03-23 17:45:01 +0100

    add user to schema

--- src/id-registry.ts
+++ src/id-registry.ts
@@ -1,4 +1,4 @@
-import { FID } from "./../generated/schema";
+import { FID, User } from "./../generated/schema";
 import { Register, Transfer } from "../generated/IdRegistry/IdRegistry";
 
 export function handleRegister(event: Register): void {
@@ -7,6 +7,8 @@ export function handleRegister(event: Register): void {
   fid.createdAtBlock = event.block.number;
   fid.createdAtTimestamp = event.block.timestamp;
   fid.save();
+
+  handleNewOwner(event.params.to.toHex(), event.params.id.toString());
 }
 
 export function handleTransfer(event: Transfer): void {
@@ -15,5 +17,29 @@ export function handleTransfer(event: Transfer): void {
   if (fid) {
     fid.custodyAddr = event.params.to;
     fid.save();
+  }
+
+  handleNewOwner(event.params.to.toHex(), event.params.id.toString());
+  handleOldOwner(event.params.from.toHex());
+}
+
+function handleNewOwner(to: string, fid: string): void {
+  let user = User.load(to);
+
+  if (!user) {
+    user = new User(to);
+    user.fname = "";
+  }
+
+  user.fid = fid;
+  user.save();
+}
+
+function handleOldOwner(from: string): void {
+  let user = User.load(from);
+
+  if (user) {
+    user.fid = "";
+    user.save();
   }
 }
--- tests/fid/fid.test.ts
+++ tests/fid/fid.test.ts
@@ -38,6 +38,19 @@ describe("Farcaster IDs", () => {
       assert.fieldEquals("FID", fid1, "createdAtBlock", "1");
       assert.fieldEquals("FID", fid1, "createdAtTimestamp", "1");
     });
+    test("should update user", () => {
+      const registerEvent = createRegisterEvent(
+        custodyAddr1,
+        fid1,
+        recoveryAddr,
+        testUrl
+      );
+      handleRegister(registerEvent);
+
+      assert.fieldEquals("User", custodyAddr1, "id", custodyAddr1);
+      assert.fieldEquals("User", custodyAddr1, "fid", fid1);
+      assert.fieldEquals("User", custodyAddr1, "fname", "");
+    });
   });
   describe("Transfer Event", () => {
     beforeAll(() => {
@@ -64,6 +77,19 @@ describe("Farcaster IDs", () => {
       assert.fieldEquals("FID", fid1, "custodyAddr", custodyAddr2);
       assert.fieldEquals("FID", fid1, "createdAtBlock", "1");
       assert.fieldEquals("FID", fid1, "createdAtTimestamp", "1");
+    });
+    test("should update user", () => {
+      handleRegister(registerEventGlobal);
+      const transferEvent = createTransferEvent(
+        custodyAddr1,
+        custodyAddr2,
+        fid1
+      );
+      handleTransfer(transferEvent);
+
+      assert.fieldEquals("User", custodyAddr2, "id", custodyAddr2);
+      assert.fieldEquals("User", custodyAddr2, "fid", fid1);
+      assert.fieldEquals("User", custodyAddr2, "fname", "");
     });
   });
 });
