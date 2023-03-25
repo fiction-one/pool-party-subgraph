c975930d3c3e45dfb99ec4f9e78eda46d16076fd
Nicky Ru <64008830+nicky-ru@users.noreply.github.com>
2023-03-25 15:35:48 +0100

    Add user count

--- src/user/helpers.ts
+++ src/user/helpers.ts
@@ -1,33 +1,24 @@
 import { FName, User, FID } from "../../generated/schema";
+import { incrementCount } from "../global/helpers";
+
+const USER_COUNT_ID = "user_count";
 
 export function updateUserFnameId(to: string, fname: FName): void {
-  let user = User.load(to);
-
-  if (!user) {
-    user = new User(to);
-  }
+  const user = loadOrCreateUser(to);
 
   user.fname = fname.id;
   user.save();
 }
 
 export function deleteUserFnameId(from: string): void {
-  let user = User.load(from);
-
-  if (!user) {
-    user = new User(from);
-  }
+  const user = loadOrCreateUser(from);
 
   user.fname = null;
   user.save();
 }
 
 export function updateUserFid(to: string, fid: FID): void {
-  let user = User.load(to);
-
-  if (!user) {
-    user = new User(to);
-  }
+  const user = loadOrCreateUser(to);
 
   user.fid = fid.id;
   user.save();
@@ -40,4 +31,15 @@ export function deleteUserFid(from: string): void {
     user.fid = null;
     user.save();
   }
+}
+
+function loadOrCreateUser(id: string): User {
+  let user = User.load(id);
+
+  if (!user) {
+    user = new User(id);
+    incrementCount(USER_COUNT_ID);
+  }
+
+  return user;
 }
